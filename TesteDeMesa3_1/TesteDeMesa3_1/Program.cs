bc66cefea73a06b57be96682c6ec565955cb0620
Marinatto <fernandomarinatto@gmail.com>
2023-10-10 00:30:56 -0300

    Ex1

---
+++ TesteDeMesa3_1/TesteDeMesa3_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Ex1
+{
+    class valorPresente
+    {
+        private double valorInicial;
+        private double taxaJuros;
+
+        public valorPresente(double valorInicial, double taxaJuros)
+        {
+            this.valorInicial = valorInicial;
+            this.taxaJuros = taxaJuros;
+        }
+        public double getValorInicial(double periodo)
+        {
+            double saldo = valorInicial * Math.Pow(1 + taxaJuros / 100, periodo);
+
+            return saldo;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira o valor inicial");
+            double valorInicial = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira a taxa de juros");
+            double taxaJuros = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira a quantidade de meses"); //pode usar de exemplo o 8,3 e afins...
+            double periodo = double.Parse(Console.ReadLine());
+
+            valorPresente tabMes = new valorPresente(valorInicial, taxaJuros);
+
+            double saldo = tabMes.getValorInicial(periodo);
+
+            Console.WriteLine($"seu saldo total é {saldo.ToString("c")}");
+        }
+
+    }
+}
